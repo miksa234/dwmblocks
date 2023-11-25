c241a5d1eb1b23514282b1b68fbfaaec972f5673
miksa234 <milutin@popovic.xyz>
2023-11-25 08:42:13 +0000

    update

--- config.h
+++ config.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	    "bar-mail",     0, 	                10              },
-	{"",	    "bar-server",   600,	            9               },
+	{"",	    "bar-mail",     5, 	                10              },
+	{"",	    "bar-bluetooth",5,  	            9               },
 	{"",	    "bar-volume",   0,	                8               },
-	{"",	    "bar-internet", 0,	                7               },
-	{"",	    "bar-vpn",      0,	                6               },
+	{"",	    "bar-internet", 5,	                7               },
+	{"",	    "bar-vpn",      10,	                6               },
 	{"",	    "bar-mem",      5,	                5               },
 	{"",	    "bar-cpu",      5,	                4               },
-/*	{"",	    "bar-battery",  180,	            3               },*/
+	{"",	    "bar-battery",  180,	            3               },
 	{"",	    "bar-weather",	600,	            2               },
-	{"",	    "bar-lang",     0,	                3               },
-	{"",	    "bar-clock",	60,	               1               },
+	{"",	    "bar-lang",     0,	                11              },
+	{"",	    "bar-clock",	60,	               1                },
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
